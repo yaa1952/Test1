b7c38737714fc0e6fbb61e99ed4a62ee5b6f8e11
yaa1952 <yaa19522007@gmail.com>
2024-01-10 20:13:53 +0300

    комментарии

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,19 +1,21 @@
 package test;
+
 import java.util.Arrays;
 
-public class Test
-{
-	private static final int	d		= 32;
-	private static final int	size	= 6;
+/**
+ * Тестовое задание от Лаборатории системных технологий
+ */
+public class Test {
+	private static final int d = 32;
+	private static final int size = 6;
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		int[] ar = randomArray(size);
 		System.out.println("Исходный массив: " + Arrays.toString(ar));
 		int dec = decode(ar);
 		System.out.println("Закодированный массив: " + dec);
-		int[]	enc		= encode(dec, ar.length);
-		String	enc_str	= Arrays.toString(enc);
+		int[] enc = encode(dec, ar.length);
+		String enc_str = Arrays.toString(enc);
 		System.out.println("Раскодированный массив: " + enc_str);
 		int proc = 100 / size;
 		System.out.println("В двоичном виде закодированный массив занимает " + proc + "% от исходного");
@@ -21,11 +23,15 @@ public class Test
 		System.out.println("В строковом виде закодированный массив занимает " + proc_str + "% от исходного");
 	}
 
-	private static int[] randomArray(int m)
-	{
-		int[]	arr			= new int[m];
-		int		minValue	= 0;
-		int		maxValue	= d - 8;
+	/**
+	 * Генерация случайного массива
+	 * @param m размер
+	 * @return
+	 */
+	private static int[] randomArray(int m) {
+		int[] arr = new int[m];
+		int minValue = 0;
+		int maxValue = d - 8;
 		for (int i = 0; i < m; i++)
 			arr[i] = minValue + (int) (Math.random() * (maxValue - minValue + 1));
 		return arr;
@@ -38,8 +44,7 @@ public class Test
 	 * @param length размер исходного массива
 	 * @return исходный массив
 	 */
-	private static int[] encode(int b, int length)
-	{
+	private static int[] encode(int b, int length) {
 		int[] arr = new int[length];
 		for (int i = 0; i < length - 1; i++) {
 			arr[length - i - 1] = (int) (b % d);
@@ -55,8 +60,7 @@ public class Test
 	 * @param ar исходный массив
 	 * @return массив закодированный в int
 	 */
-	private static int decode(int[] ar)
-	{
+	private static int decode(int[] ar) {
 		int b = ar[0];
 		for (int i = 0; i < ar.length - 1; i++)
 			b = b * d + ar[i + 1];
